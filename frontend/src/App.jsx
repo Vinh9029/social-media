4005b05cf50025009993b520dab8e8e06245691e
Vinh Dep Trai <bearastrikingresemblance@gmail.com>
2026-02-07 23:03:44 +0700

    main

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">ôi 
+      <h1>Social Website</h1>
+      <p>Welcome to your new project!</p>
+    </div>
+  )
+}
+
+export default App
